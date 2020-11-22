c995d98298dce25d8fda60cb878d25124927894c
Steffan.Crowe <fub4r@hotmail.co.uk>
2020-11-22 00:53:34 +0000

    AutoConsult portfolio template
    Started work on AutoConsult page

--- steffan-cv/src/Pages/AutoConsult.js
+++ steffan-cv/src/Pages/AutoConsult.js
@@ -1,26 +1,136 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
-import Header from '../Components/Header';
-import Footer from '../Components/Footer';
-import About from '../Components/About';
-import Resume from '../Components/Resume';
-import Portfolio from '../Components/Portfolio';
-import Contact from '../Components/Contact';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Link } from "react-router-dom";
 
-class AutoConsult extends Component {
-
-  render() {
-    return (
-        <header id="home">
-            <div className="row banner">
-            <div className="banner-text">
-                <h1 className="responsive-headline">AutoConsult</h1>
-            </div>
-            </div>
-        </header>
-    );
-  }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+        Steffan Crowe  
+      {2020}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default AutoConsult;
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(4, 0, 2,)
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function AutoConsult() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+        <Toolbar>
+            <Link title="Back" to="">  
+            <Grid container spacing={2} justify="left">
+                <Grid item>
+                    <i className="icon-left-open" />
+                </Grid>
+                <Grid item>
+                    <Typography variant="h4" color="inherit" noWrap>
+                        Back
+                    </Typography>
+                </Grid>
+              </Grid>            
+            </Link>
+        </Toolbar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              AutoConsult
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              AutoConsult is a business that lease cars founded by some friends of mine.
+              They required a system for their employees to use, to allow them
+              to manage their customers and the status of their lease applications.
+            </Typography>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="images/portfolio/autoconsult-login.JPG"
+                    title="User Login"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      User Login
+                    </Typography>
+                    <Typography>
+                      Every employee has their own login details to access the system, 
+                      with differing levels of privileges.
+                      Using Simblas database driven website builder, storing the user
+                      account details is made secure and easy to manage.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
